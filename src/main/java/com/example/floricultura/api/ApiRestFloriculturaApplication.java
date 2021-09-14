3f86a2fe340220331b9b9e3dcff9d1da5df8748b
Dumilde Matos <dumildejosematos@gmail.com>
2021-09-14 09:04:32 +0100

    Initial Commit

---
+++ src/main/java/com/example/floricultura/api/ApiRestFloriculturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.floricultura.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFloriculturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestFloriculturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/floricultura/api/ApiRestFloriculturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.floricultura.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFloriculturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
